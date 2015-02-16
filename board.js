95b929a694fa1356688d88d5bca606579291fd14
koek67 <krishnan.koushik@gmail.com>
2015-02-16 15:45:25 -0500

    added IDE

--- board.js
+++ board.js
@@ -1,21 +1,24 @@
+/**
+ * Created by koushikkrishnan on 2/15/15.
+ */
 /*
-* Class Board
-*
-* Board will store all the information for the game of Minesweeper. This
-* includes all the rules, flags, bombs, and the information in the grid.
-*
-* Board will have three 2D arrays that will act as layers:
-*
-*  bombs - the locations of all the bombs
-*  buttons - the places the user has clicked
-*  flags - the places a flag has been placed
-*  bombNums - the the number of bomb neighbors for each location
-*
-* Board will handle all the logic of the minesweeper game using these layers
-* and it will update thse layers everytime a user buttons on a location on the
-* game board.
-*
-*/
+ * Class Board
+ *
+ * Board will store all the information for the game of Minesweeper. This
+ * includes all the rules, flags, bombs, and the information in the grid.
+ *
+ * Board will have three 2D arrays that will act as layers:
+ *
+ *  bombs - the locations of all the bombs
+ *  buttons - the places the user has clicked
+ *  flags - the places a flag has been placed
+ *  bombNums - the the number of bomb neighbors for each location
+ *
+ * Board will handle all the logic of the minesweeper game using these layers
+ * and it will update thse layers everytime a user buttons on a location on the
+ * game board.
+ *
+ */
 function Board () {
 
     // set the difficulty to intermedite
@@ -23,15 +26,16 @@ function Board () {
     this.numCols = 16;
 
     this.numBombs = 99;
-    this.numFlags = numBombs;
+    this.numFlags = this.numBombs;
 
     // set up the layers
-    this.bombs = [[]];
-    this.buttons = [[]];
-    this.flags = [[]];
-    this.bombNums =[[]];
+    this.bombs = new Array();
+    this.buttons = new Array(this.numRows);
+    this.flags = new Array(this.numRows);
+    this.bombNums = new Array(this.numRows);
 
     // properties of the game
+    this.firstClick = true;
     this.endGame = false;
     this.win = true;
 
@@ -42,43 +46,59 @@ function Board () {
      * be filled because it requires the first click of the user.
      */
     this.init = function() {
-        fillButtons();
-        fillFlags();
+        this.fillButtons();
+        this.fillFlags();
+        this.fillBombsEmpty();
     }
 
-    this.fillbuttons = function() {
-        for (var i = 0; i < numRows; i++) {
-            for (var j = 0; j < numCols; j++) {
-                buttons[i][j] = false;
+    this.fillButtons = function() {
+        for(var i = 0; i < this.numRows; i++) {
+            var subArray = new Array();
+            for (var j = 0; j < this.numCols; j++) {
+                subArray.push(false);
             }
+            this.buttons.push(subArray);
+        }
+    }
+
+    this.fillBombsEmpty = function() {
+        console.log("called");
+        for(var i = 0; i < this.numRows; i++) {
+            var subArray = new Array();
+            for (var j = 0; j < this.numCols; j++) {
+                subArray.push(false);
+            }
+            this.bombs.push(subArray);
         }
     }
 
     this.fillFlags = function() {
-        for(var i = 0; i < numRows; i++) {
-            for (var j = 0; j < numCols; j++) {
-                flags[i][j] = false;
+        for(var i = 0; i < this.numRows; i++) {
+            var subArray = new Array();
+            for (var j = 0; j < this.numCols; j++) {
+                subArray.push(false);
             }
+            this.flags.push(subArray);
         }
     }
 
-    this.fillBombs = function(var x, var y) {
+    this.fillBombs = function(x, y) {
         var bombsPlaced = 0;
-        while (bombsPlaced < numBombs) {
-            var r = Math.random() * numRows;
-            var c = Math.random() * numCols;
-            if (r != x && c != y && bombs[i][j] == false) {
-                bombs[i][j] = true;
+        while (bombsPlaced < this.numBombs) {
+            var r = Math.floor(Math.random() * this.numRows);
+            var c = Math.floor(Math.random() * this.numCols);
+            if (r != x && c != y && this.bombs[r][c] == false) {
+                this.bombs[r][c] = true;
                 bombsPlaced++;
             }
         }
     }
 
-    this.isLegal = function(var x, var y) {
-        return x < numRows && y < numCols && x >= 0 && y >= 0;
+    this.isLegal = function(x, y) {
+        return x < numRows && y < this.numCols && x >= 0 && y >= 0;
     }
 
-    this.expand = function(var x, var y) {
+    this.expand = function(x, y) {
         var empty = [[]]
         if (!isLegal(x,y)) {
             return empty;
@@ -106,12 +126,11 @@ function Board () {
             for ( var p in points )
                 if (p[0] != x && p[1] != y)
                     expand(p[0],p[1]);
-
-                }
+        }
 
     }
 
-    this.getNeighbors = function(var x, var y) {
+    this.getNeighbors = function(x, y) {
         var points = [[]];
         var count = 0;
         for (var i = -1; i < 1; i++) {
@@ -119,10 +138,35 @@ function Board () {
                 if (isLegal(x + i, y + j)) {
                     points[count][0] = x + i;
                     points[count][1] = y + j;
+                    count++;
                 }
             }
         }
         return points;
-        }
+    }
 
+    this.showAllBombs = function() {
+        // iterate through the buttons with bombs
+        // under them and click them
+        for (var i = 0; i < this.buttons.length; i++) {
+            for (var j = 0; j < this.buttons[0].length; j++) {
+                // check if there is a bomb there
+                if (this.bombs[i][j] == true) {
+                    // click the button here
+                    this.buttons[i][j] = true;
+                }
+            }
+        }
+    }
+
+    this.click = function(row, col) {
+        // handle cases
+        // is there a bomb there?
+        if (bombs[row][col] == true) {
+            this.endGame = true;
+            this.win = false;
+            this.showAllBombs();
+        }
+        // is
+    }
 }
